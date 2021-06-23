46a50f283ff1f3b3a2736af510bfff3e4107612d
Raj Kumar <interplaytheheads@gmail.com>
2021-06-23 16:10:22 +0300

    database config, errors, ports included

--- index.js
+++ index.js
@@ -1,14 +1,81 @@
-const http = require('http');
+let express = require('express');
+let mongoose = require('mongoose');
+let cors = require('cors');
+let bodyParser = require('body-parser');
+let dbConfig = require('./database/db');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// Error define
+const createError = require('http-errors');
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-});
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+// Express Routing
+
+const venueRoute = require('../booking_backend/routes/venue.route.js')
+
+// Connecting mongoDB Database
+
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.db, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log('Database successfully connected!');
+},
+    error => {
+    console.log('Could not connect to database: ' + error)
+})
+
+
+// using express
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+
+app.use(cors());
+app.use('/venues', venueRoute);
+
+// server port definition
+const port = process.env.PORT || 4000;
+const server = app.listen(port, () => {
+    console.log('Connected to port' + port)
+})
+
+// 404 Error
+app.use((req, res, next) => {
+    next(createError(404));
+  });
+  
+app.use(function (err, req, res, next) {
+    console.error(err.message);
+    if (!err.statusCode) err.statusCode = 500;
+    res.status(err.statusCode).send(err.message);
+  });
+
+
+
+
+
+
+
+
+
+
+// ctrl + K + C to comment 
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
