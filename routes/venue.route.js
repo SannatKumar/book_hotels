51418579095e7a168d0b64d8e99399eb6ba7f087
Raj Kumar <interplaytheheads@gmail.com>
2021-06-23 15:42:58 +0300

    routes created for creating and viewing the data

--- routes/venue.route.js
+++ routes/venue.route.js
@@ -4,5 +4,31 @@ router = express.Router();
 
 // Venue Model
 
-let studentSchema = require('../models/venuedb');
+let venueSchema = require('../models/venuedb');
 
+// CREATE venues data
+router.route('/create-venue').post((req, res, next) => {
+    venueSchema.create(req.body, (error, data) => {
+      if (error) {
+        return next(error)
+      } else {
+        console.log(data)
+        res.json(data)
+      }
+    })
+  });
+
+  //Read the data
+
+router.route('/').get((req, res) =>{
+    venueSchema.find((error, data) => {
+        if(error) {
+            return next(error)
+        }else {
+            res.json(data)
+        }
+    })
+})
+
+
+module.exports = router;
