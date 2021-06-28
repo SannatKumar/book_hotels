61082577d45d2822c1fa9e0603fba54122b83656
Raj Kumar <interplaytheheads@gmail.com>
2021-06-28 07:06:25 +0300

    Final routes and code cleaned and console.log removed

--- routes/venue.route.js
+++ routes/venue.route.js
@@ -6,9 +6,10 @@ router = express.Router();
 
 let {venue, room, website, venueWebsite, bookingData} = require('../models/venuedb');
 
+
+
 // Create venue data
 router.route('/create-venue').post(async (req, res, next) => {
-  console.log("create-venue_route");
   try{
     const hotelName = req.body;
     const venueCreation = await venue.create(hotelName);
@@ -22,7 +23,6 @@ router.route('/create-venue').post(async (req, res, next) => {
 // Read the venue Data
 
 router.route('/show-venue').get(async (req, res, next) => {
-  console.log("show-venue_route");
   try{
     const venueData = await venue.find();
     return res.json(venueData);
@@ -34,7 +34,6 @@ router.route('/show-venue').get(async (req, res, next) => {
 
 // Create meeting Room data
 router.route('/create-meeting-room').post(async (req, res, next) => {
-  console.log("create-meeting_room");
   try{
     const {name, venue} = req.body;
     const roomCreation = await room.create({name: name, venue: venue});
@@ -48,7 +47,6 @@ router.route('/create-meeting-room').post(async (req, res, next) => {
 // Read the meetingRoom Data
 
 router.route('/show-meeting-room').get(async (req, res, next) => {
-  console.log("show-meeting_room");
   try{
     const roomData = await room.find();
     return res.json(roomData);
@@ -58,242 +56,111 @@ router.route('/show-meeting-room').get(async (req, res, next) => {
   }
 });
 
+// Create Website data
+router.route('/create-website').post(async (req, res, next) => {
+  try{
+    const {name} = req.body;
+    const websiteCreation = await website.create({websiteName: name});
+    return res.json(websiteCreation);
+  }
+  catch(error){
+    return next(error);
+  }
+});
+
+// Read the website Data
+
+router.route('/show-website-data').get(async (req, res, next) => {
+  try{
+    const websiteData = await website.find();
+    return res.json(websiteData);
+  }
+  catch(error){
+    return next(error);
+  }
+});
+
+// Create venue Website data
+router.route('/create-venue-website').post(async (req, res) => {
+  try{
+    const {venue, website, isVisible} = req.body;
+    const venueWebsiteCreation = await venueWebsite.create({venue: venue, website: website, isVisible: isVisible});
+    return res.json(venueWebsiteCreation);
+  }
+  catch(error){
+    res.send({"Error": "something went wrong!"});
+  }
+});
+
+// Read the venue website Data
+router.route('/show-venue-website-data').get(async (req, res) => {
+  var populateQuery = [
+    {path:'venue', select:'name'},
+    {path:'meetingRoom', select:'name venue'},
+    {path: 'website', select: 'websiteName'},
+    {path: 'venueWebsite', select: 'venue website isVisible'}
+  ];
+  try{
+    const venueWebsiteData = await venueWebsite.find().populate(populateQuery);
+    return res.json(venueWebsiteData);
+  }
+  catch(error){
+    res.send({"Error": "something went wrong!"});
+  }
+});
+
+// Create booking Data
+router.route('/create-booking-data').post(async (req, res, next) => {
+  try{
+    const {venueWebsite, room, price} = req.body;
+    const bookingVenueData = await bookingData.create({
+      venueWebsite: venueWebsite,
+      room: room,
+      price: price
+    });
+    return res.json(bookingVenueData);
+  }
+  catch(error){
+    return next(error);
+  }
+});
+
+// Read the booking Data
+
+router.route('/show-booking-data').get(async (req, res, next) => {
+  var populateQuery = [
+    {path:'venue', select:'name'},
+    {path:'meetingRoom', select:'name venue'},
+    {path: 'website', select: 'websiteName'},
+    {path: 'venueWebsite', select: 'venue website isVisible'},
+    {path: 'bookingData', select: 'venueWebsite room price'}
+  ];
+  try{
+    const retrBookingData = await bookingData.find().populate(populateQuery);;
+    return res.json(retrBookingData);
+  }
+  catch(error){
+    return next(error);
+  }
+});
 
 
+// Read the website data based on single website id
 
-
-
-//   //Read the hotel data
-
-// router.route('/show-hotel').get((req, res) =>{
-//   hotel.find((error, data) => {
-//       if(error) {
-//           return next(error)
-//       }else {
-//           res.json(data)
-//       }
-//   })
-// });
-
-
-//   // Create Website data
-//   router.route('/create-website').post((req, res, next) => {
-//     website.create(req.body, (error, data) => {
-//       if (error) {
-//         return next(error)
-//       } else {
-//         console.log(data)
-//         res.json(data)
-//       }
-//     })
-//   });
-
-//   //Read the website data
-
-//   router.route('/show-website').get((req, res) => {
-//     website.find((error, data)=> {
-//       if(error){
-//         return next(error)
-//       }
-//       else{
-//         return res.json(data);
-//       }
-//     })
-//   })
-
-//    // Create booking data
-//    router.route('/create-booking').post((req, res, next) => {
-//     bookingdata.create(req.body, (error, data) => {
-//       if (error) {
-//         return next(error)
-//       } else {
-//         console.log(data)
-//         res.json(data)
-//       }
-//     })
-//   });
-
-//   //Read the booking data
-
-//   router.route('/show-booking').get((req, res) => {
-//     var populateQuery = [
-//       {path:'website', select:'domainName domain'},
-//       {path:'hotel', select:'name'},
-//       {path: 'room', select: 'name roomNumber hotel'}
-//     ];
-//     bookingdata.find((error, data)=> {
-//       if(error){
-//         return next(error)
-//       }
-//       else{
-//         return res.json(data);
-//       }
-//     }).populate(populateQuery) 
-//   })
-
-
-
-//   // Create room data
-//   router.route('/create-room').post((req, res, next) => {
-//     room.create(req.body, (error, data) => {
-//       if (error) {
-//         return next(error)
-//       } else {
-//         console.log(data)
-//         res.json(data)
-//       }
-//     })
-//   });
-
-//     //Read the room data
-
-//     router.route('/show-room').get((req, res) => {
-//       room.find((error, data)=> {
-//         if(error){
-//           return next(error)
-//         }
-//         else{
-//           return res.json(data);
-//         }
-//       }).populate('hotel')
-//     })
-
-
-  
-
-
-
-  
-
-// //REad the hotel website data
-
-// router.route('/show-hotelwebsite').get((req, res) =>{
-//   hotelWebsite.find((error, data) => {
-//       if(error) {
-//           return next(error)
-//       }else {
-//           res.json(data)
-//       }
-//   })
-// })
-
-
-// //create hotelwebsite data
-// router.route('/create-hotelwebsite').post((req, res, next) => {
-//   hotelWebsite.create(req.body, (error, data) => {
-//     if (error) {
-//       return next(error)
-//     } else {
-//       console.log(data)
-//       res.json(data)
-//     }
-//   })
-// });
-
-// // router.route('/websited/:id').get((req, res) => {
-// //   console.log(req.params.id)
-// //   const websiteId = req.params.id;
-// //   console.log(typeof websiteId)
-// //   hotelWebsite.findOne({"website": {_id: websiteId}, isVisible: true},(error, data) =>{
-// //     if(error){
-// //       return res.json({error: "Object Does not exist"})
-// //     }
-// //     if (!data){return res.send("No rooms found")}
-// //     bookingdata.find({"hotelWebsite": {_id: data._id}}, (error, exactData) => {
-// //       if(error){
-// //         return res.json({error: "Fail to Load"})
-// //       }
-// //       console.log(exactData);
-// //       return res.json(exactData);
-// //     })
-
-// //   })
-// // });
-
-// // // Async await True
-// // router.route('/website/:id').get(async (req, res) => {
-// //   const websiteId = req.params.id;
-// //   console.log(typeof websiteId)
-// //   try{
-// //     const hotelwebsiteInstance = await hotelWebsite.findOne({"website": {_id: websiteId}, isVisible: true});
-// //     return res.json(hotelwebsiteInstance);
-// //   }
-// //   catch(error)
-// //   {
-// //     res.send({error: "Fail to Load"})
-// //   }
-// //   if (!hotelwebsiteInstance){
-// //     return res.send("No data found")
-// //   }
-// //   try {
-// //     const bookingdataInstance = await bookingdata.find({"hotelWebsites": {_id: hotelwebsiteInstance._id}});
-// //     return res.json(bookingdataInstance)
-// //   }
-// //   catch (error) {
-// //     console.log('Error here ', error);
-// //     return res.send({})
-// //   }
-// // });
-
-// // Async await False
-// router.route('/website/:id').get(async (req, res) => {
-//   const websiteId = req.params.id;
-//   try{
-//     const hotelwebsiteInstance = await hotelWebsite.findOne({"website": {_id: websiteId}, isVisible: true});
-//     if (!hotelwebsiteInstance){
-//       return res.send("No data found")
-//     }
-//     else{
-//       try{
-//         const bookingdataInstance = await bookingdata.find({"hotelWebsites": {_id: hotelwebsiteInstance._id}});
-//         return bookingdataInstance;
-//       }
-//       catch(error){
-//         return res.send(error);
-//       }
-//     }
-//   }
-//   catch(error)
-//   {
-//     res.send({error: "Fail to Load"})
-//   }
-// });
-
-
-// //Read the booking data
-
-// //REad the hotel website data
-
-// router.route('/show-bookingdata').get(async(req, res) =>{
-//   var spopulateQuery = [
-//     {path:'website', select:'domainName domain'},
-//     {path:'hotel', select:'name'},
-//     {path: 'bookingdata', select: 'price'}
-//   ];
-//   try{
-//     const showVisibileWebsite = await hotelWebsite.find({isVisible: true}).populate(spopulateQuery);
-//     const showVisibileWebsiteId = res.json(showVisibileWebsite);
-//     const visibleDataPrice = await bookingdata.find({"bookingdata": {_id: showVisibileWebsiteId.website.id}});
-//     return res.json(visibleDataPrice);
-//   }
-//   catch(error)
-//   {
-//     res.send({error: "Fail to Load"})
-//   }
-
-// });
-// //Read the website data
-
-// // router.route('/website').get((req, res) =>{
-// //     websites.find((error, data) => {
-// //         if(error) {
-// //             return next(error)
-// //         }else {
-// //             res.json(data)
-// //         }
-// //     })
-// // })
-
+router.route('/website/:id').get(async (req, res) => {
+  const websiteId = req.params.id;
+  try{
+    const visibleVenueWebsite = await venueWebsite.findOne({"website": {_id: websiteId}, isVisible: true});
+    const bookingdataInstance = await bookingData.find(
+          {"venueWebsite": {_id: visibleVenueWebsite._id}});
+          console.log("Booking Data Display", bookingdataInstance);
+        return res.json(bookingdataInstance)  
+  }
+  catch(error)
+  {
+    return res.send({error: "Fail to Load"});
+  }
+});
 
 module.exports = router;
 
